36807ba06fa5909a499c0bc350a4a0061daddc01
ahmed <ahmad.abdelhamid@gmail.com>
2023-04-19 12:50:38 +0200

    init

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.mockito.Mockito.*;
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+}
